35c9724f84426feaacec0e2773ca429cbf3b5cb8
James Webb <james@ultra-horizon.com>
2018-02-26 14:41:45 +0000

    Reformat

--- cw2/att1.py
+++ cw2/att1.py
@@ -6,36 +6,40 @@
 
 import sys, subprocess
 
-def interact( G ) :
-  # Send G to attack target.
-  target_in.write( "%s\n" % ( G ) ) ; target_in.flush()
 
-  # Receive ( t, r ) from attack target.
-  t = int( target_out.readline().strip() )
-  r = int( target_out.readline().strip() )
-  return ( t, r )
+def interact(G):
+    # Send G to attack target.
+    target_in.write("%s\n" % (G));
+    target_in.flush()
 
-def attack() :
-  # Select a hard-coded guess ...
-  G = "guess"
+    # Receive ( t, r ) from attack target.
+    t = int(target_out.readline().strip())
+    r = int(target_out.readline().strip())
+    return (t, r)
 
-  # ... then interact with the attack target.
-  ( t, r ) = interact( G )
 
-  # Print all of the inputs and outputs.
-  print "G = %s" % ( G )
-  print "t = %d" % ( t )
-  print "r = %d" % ( r )
+def attack():
+    # Select a hard-coded guess ...
+    G = "guess"
 
-if ( __name__ == "__main__" ) :
-  # Produce a sub-process representing the attack target.
-  target = subprocess.Popen( args   = sys.argv[ 1 ],
-                             stdout = subprocess.PIPE,
-                             stdin  = subprocess.PIPE )
+    # ... then interact with the attack target.
+    (t, r) = interact(G)
 
-  # Construct handles to attack target standard input and output.
-  target_out = target.stdout
-  target_in  = target.stdin
+    # Print all of the inputs and outputs.
+    print "G = %s" % (G)
+    print "t = %d" % (t)
+    print "r = %d" % (r)
 
-  # Execute a function representing the attacker.
-  attack()
+
+if (__name__ == "__main__"):
+    # Produce a sub-process representing the attack target.
+    target = subprocess.Popen(args=sys.argv[1],
+                              stdout=subprocess.PIPE,
+                              stdin=subprocess.PIPE)
+
+    # Construct handles to attack target standard input and output.
+    target_out = target.stdout
+    target_in = target.stdin
+
+    # Execute a function representing the attacker.
+    attack()
