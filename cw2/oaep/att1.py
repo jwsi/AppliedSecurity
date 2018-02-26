f3c84be1ad2b8b192816bbb06ac7a8086d1e62f5
James Webb <james@ultra-horizon.com>
2018-02-26 17:13:54 +0000

    Remove global variables

--- cw2/oaep/att1.py
+++ cw2/oaep/att1.py
@@ -1,11 +1,8 @@
-# Copyright (C) 2017 Daniel Page <csdsp@bristol.ac.uk>
-#
-# Use of this source code is restricted per the CC BY-NC-ND license, a copy of
-# which can be found via http://creativecommons.org (and should be included as
-# LICENSE.txt within the associated archive or repository).
+import sys, subprocess, math
 
-import sys, subprocess
 
+
+# This function extracts parameters from a config file
 def getParams(file):
     print "Attempting to parse the config file..."
     conf = open(file, "r")
@@ -13,36 +10,41 @@ def getParams(file):
     print "N (RSA Modulus): " + str(N)
     e = int(conf.readline(), 16)
     print "e (public exponent): " + str(e)
-    l = conf.readline()[:-1]
+    l = conf.readline()[:-1] # remove newline
     print "l (octal OAEP label): " + str(l)
-    c = conf.readline()[:-1]
+    c = conf.readline()[:-1] # remove newline
     print "c (octal ciphertext): " + str(c)
     conf.close()
     return (N, e, l, c)
 
-def communicate(l, c):
-    # Send G to attack target.
+
+# This function communicates with the attack target
+def communicate(target, l, c):
+    # Send label & ciphertext to attack target.
     target.stdin.write(l + "\n")
     target.stdin.write(c + "\n")
     target.stdin.flush()
 
-    # Receive ( t, r ) from attack target.
+    # Receive result code from attack target.
     result = int(target.stdout.readline().strip())
     return result
 
-if (__name__ == "__main__"):
-    # Read in and parse the config file
-    (N, e, l, c) = getParams(sys.argv[2])
 
-    print sys.argv[1]
+# This is the main function
+def main():
+    # Parse the config file.
+    (N, e, l, c) = getParams(sys.argv[2])
+    # Construct the variables required for an attack.
+    k = int(math.ceil(math.log(N, 256)))
+    B = 2 ** (8 * (k - 1))
+
+    # Spin up a subprocess.
     target = subprocess.Popen(args=["noah", sys.argv[1]], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 
-    result = communicate(l, c)
+    # Begin the attack
+    result = communicate(target, l, c)
     print result
-    #
-    # # Construct handles to attack target standard input and output.
-    # target_out = target.stdout
-    # target_in = target.stdin
-    #
-    # # Execute a function representing the attacker.
-    # attack()
+
+
+if (__name__ == "__main__"):
+    main()
