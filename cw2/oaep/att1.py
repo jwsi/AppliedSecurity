7a2e900f4893fe6c943d2c43e7dfb31b931e8587
James Webb <james@ultra-horizon.com>
2018-03-03 21:57:49 +0000

    Improve printouts, code resilience  and documentation

--- cw2/oaep/att1.py
+++ cw2/oaep/att1.py
@@ -3,6 +3,7 @@ import sys, subprocess, math, hashlib
 # Define global variable for interactions with oracle
 interactions = 0
 
+
 # This function extracts parameters from a config file
 def getParams(file):
     print "Attempting to parse the config file..."
@@ -14,7 +15,7 @@ def getParams(file):
     l = int(conf.readline().strip(), 16)
     print "l (octet OAEP label): " + "{0:X}".format(l)
     c = int(conf.readline().strip(), 16)
-    print "c (octet ciphertext): " + "{0:0256X}".format(c)
+    print "c (octet ciphertext): " + "{0:0256X}".format(c) + "\n"
     conf.close()
     return (N, e, l, c)
 
@@ -24,7 +25,7 @@ def communicate(target, l, c):
     global interactions
     # Send label & ciphertext to attack target.
     ctxt  = "{0:0256X}".format(c)
-    label = "{0:0256X}".format(l)
+    label = "{0:X}".format(l)
     target.stdin.write(label + "\n")
     target.stdin.write(ctxt  + "\n")
     target.stdin.flush()
@@ -37,7 +38,8 @@ def communicate(target, l, c):
 
 # This function performs step 1 from Manger's paper
 def step1(target, e, c, l, N):
-    print "Starting step 1 of the attack..."
+    print "Starting step 1 of the attack... ",
+    startInteractions = interactions
     f1 = 1
     resultCode = 0
     while resultCode != 1:
@@ -45,13 +47,14 @@ def step1(target, e, c, l, N):
         challenge  = pow(f1, e, N)
         challenge  = (challenge * c) % N
         resultCode = communicate(target, l, challenge)
-    print "f1 = " + str(f1) + "\n"
+    print "COMPLETE! This took " + str(interactions - startInteractions) + " oracle queries."
     return f1
 
 
 # This function performs step 2 from Manger's paper
 def step2(target, l, f1, e, c, N, B):
-    print "Starting step 2 of the attack..."
+    print "Starting step 2 of the attack... ",
+    startInteractions = interactions
     f2 = divFloor((N+B), B) * (f1 / 2)
     challenge  = pow(f2, e, N)
     challenge  = (challenge * c) % N
@@ -61,13 +64,14 @@ def step2(target, l, f1, e, c, N, B):
         challenge  = pow(f2, e, N)
         challenge  = (challenge * c) % N
         resultCode = communicate(target, l, challenge)
-    print "f2 = " + str(f2) + "\n"
+    print "COMPLETE! This took " + str(interactions - startInteractions) + " oracle queries."
     return f2
 
 
 # This function performs step 3 from Manger's paper
 def step3(target, l, f2, e, c, N, B, k):
-    print "Starting step 3 of the attack..."
+    print "Starting step 3 of the attack... ",
+    startInteractions = interactions
     m_min = divCeil ( N    , f2 )
     m_max = divFloor( (N+B), f2 )
     while m_min != m_max:
@@ -83,7 +87,7 @@ def step3(target, l, f2, e, c, N, B, k):
             m_min = divCeil ( (i * N + B), f3 )
         else:
             m_max = divFloor( (i * N + B), f3 )
-    print "f3 = " + str(f3)
+    print "COMPLETE! This took " + str(interactions - startInteractions) + " oracle queries."
     print "encoded plaintext (octet string): " + "{0:0256X}".format(m_max) + "\n"
     return m_max
 
@@ -104,7 +108,9 @@ def divCeil(a, b):
     return multiple/b + 1
 
 
+# This function decodes an OAEP encoded message into plaintext
 def OAEPDecode(EM, L):
+    print "Decoding plaintext... ",
     EM = "{0:0256X}".format(EM)
     if L is None:
         L = ""
@@ -113,7 +119,7 @@ def OAEPDecode(EM, L):
     lHash = hashlib.sha1(L.decode('hex')).hexdigest().upper()
     hLen = len(lHash)
     Y = EM[0:2]
-    maskedSeed = EM[2:hLen+2] # SHA1 = 160bit => 40 hex symbols
+    maskedSeed = EM[2:hLen+2]
     maskedDB = EM[hLen+2:]
     seedMask = MGF1(maskedDB, hLen)
     seed = hexXOR(maskedSeed, seedMask)
@@ -121,16 +127,17 @@ def OAEPDecode(EM, L):
     DB = hexXOR(maskedDB, dbMask)
 
     lHashMK2 = DB[:40]
-    paddedM = DB[40:].split("01")
+    paddedM = DB[40:].split("01", 1) # Split on first occurrence of padding
 
-    # # Checks
+    # Checks
     if Y != "00" or lHash != lHashMK2 or len(paddedM) < 2:
         raise Exception("decryption error")
+    # Return the message
+    print "COMPLETE!"
+    return paddedM[1]
 
-    # Output message
-    print paddedM[-1]
 
-
+# This function implements the MGF1 mask function
 def MGF1(Z, l):
     hLen = hashlib.sha1("").digest_size * 2 # Digest size of SHA1 in hex
     if l > pow(2, 32) * hLen:
@@ -140,9 +147,10 @@ def MGF1(Z, l):
     for counter in range(0, divCeil(l, hLen)):
         C = "{0:08X}".format(counter)
         T = T + hashlib.sha1((Z + C).decode('hex')).hexdigest().upper()
-    return T[0:l]
+    return T[:l]
 
 
+# This function implements XOR of hex strings and pads to fit input length
 def hexXOR(a, b):
     a_int = int(a, 16)
     b_int = int(b, 16)
@@ -162,10 +170,12 @@ def main():
     f1 = step1(target, e, c, l, N)
     f2 = step2(target, l, f1, e, c, N, B)
     encoded_m = step3(target, l, f2, e, c, N, B, k)
-    OAEPDecode(encoded_m, l)
     # Decode the message
+    m = OAEPDecode(encoded_m, l)
+    # Print the plaintext message
+    print "Plaintext message: " + m
     # Print the number of oracle interactions required
-    print interactions
+    print "Total oracle interactions: " + str(interactions)
 
 
 if (__name__ == "__main__"):
