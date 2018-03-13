aa2c4336839d25a4cc37fa0fb3a486d53005d0df
James Webb <james@ultra-horizon.com>
2018-03-13 12:17:24 +0000

    Added hard reset code...

--- cw2/time/att2.py
+++ cw2/time/att2.py
@@ -70,9 +70,9 @@ def analyse(M1, M2, M3, M4):
     avgF4 = float(sum(M4.values()))/len(M4)
     diff1 = abs(avgF1 - avgF2)
     diff2 = abs(avgF3 - avgF4)
-    if (diff1 > diff2) and avgF1 > avgF2 and abs(avgF3 - avgF4) < 50:
+    if (diff1 > diff2) and avgF1 > avgF2:
         return "1"
-    elif (diff2 > diff1) and avgF3 > avgF4 and abs(avgF1 - avgF2) < 50:
+    elif (diff2 > diff1) and avgF3 > avgF4:
         return "0"
     else:
         return "warn"
@@ -110,7 +110,11 @@ def attack(target, N, e):
         else: # Error detected restart the operation...
             print "ERROR DETECTED! Rebuilding key..."
             b = "1"
-            messages.update(generateMessages(500, target, N))
+            if len(messages) < 10000:
+                messages.update(generateMessages(1000, target, N))
+            else:
+                print "CLEARING MESSAGE SET..."
+                messages = generateMessages(4000, target, N)
             mTemps = {m: montgomeryForm( pow(pow(m, int(b, 2), N), 2, N) , N, R) for m in messages.keys()}
             continue
         correct, key = correctKey(target, b, N)
@@ -122,18 +126,21 @@ def attack(target, N, e):
 
 
 def generateMessages(amount, target, N):
+    print "Generating " + str(amount) + " messages... ",
     random.seed() # Seed internal PRNG with current time (don't really care too much about true randomness here...)
     messages = {}
     m = random.getrandbits(N.bit_length())
-    res, _ = communicate(target, m)
+    res, time = communicate(target, m)
+    messages[m] = time
     global groundTruth
     groundTruth = (m, res)
-    while(len(messages) != amount-1):
+    while(len(messages) != amount):
         m = random.getrandbits(N.bit_length())
         while (m >= N):
             m = random.getrandbits(N.bit_length())
         res, time = communicate(target, m)
         messages[m] = time
+    print "COMPLETE!"
     return messages
 
 
