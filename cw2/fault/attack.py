01464407cd1da5f0d3112a9b89990bd8dac74047
James Webb <james@ultra-horizon.com>
2018-04-05 15:18:18 +0100

    Added SBOX and GF8 multiplication

--- cw2/fault/attack.py
+++ cw2/fault/attack.py
@@ -1,25 +1,60 @@
-import sys, subprocess
+import sys, subprocess, random
 from fault import Fault
+from sbox import s, sInv
 
 # Define global variable for interactions with oracle
 interactions = 0
 
 
 # This function communicates with the attack target
-def communicate(target, m, f):
+def communicate(target, message, fault):
     global interactions
     # Send label & ciphertext to attack target.
-    ctxt  = "{0:0256X}".format(c)
-    label = "{0:X}".format(l)
-    target.stdin.write(label + "\n")
-    target.stdin.write(ctxt  + "\n")
+    messageString  = "{0:X}".format(message)
+    faultString = fault.description()
+    target.stdin.write(faultString + "\n")
+    target.stdin.write(messageString  + "\n")
     target.stdin.flush()
 
     # Receive result code from attack target.
-    result = int(target.stdout.readline().strip())
+    ctxt = int(target.stdout.readline().strip())
     interactions += 1
-    return result
+    return ctxt
 
+
+def generateMessages(amount):
+    print "Generating " + str(amount) + " messages... ",
+    random.seed() # Seed internal PRNG with current time (don't really care too much about true randomness here...)
+    messages = []
+    while(len(messages) != amount):
+        m = random.getrandbits(128)
+        while (m.bit_length() != 128):
+            m = random.getrandbits(128)
+        messages.append(m)
+    print "COMPLETE!"
+    return messages
+
+
+# Modified peasants algorithm to perform multiplication in GF8 under the Rijndael irreducible polynomial.
+def gf8Multiply(a, b):
+    # Setup initial values + ensure 8 bit max
+    p = 0
+    a = a & 255
+    b = b & 255
+    for i in range(8):
+        # If the rightmost bit of b is set, exclusive OR the product p by the value of a.
+        if (b & 1) == 1:
+            p = p ^ a
+        # Shift b one bit to the right
+        b = b >> 1
+        # Is leftmost bit of a set to 1?
+        carry = (a & 128) >> 7
+        # Shift a one bit to the left, discarding the leftmost bit, and making the new rightmost bit zero
+        a = (a << 1) & 255
+        # If carry had a value of one, exclusive or a with the hexadecimal number 0x1b. 0x1b corresponds to the irreducible polynomial with the high term eliminated.
+        if carry:
+            a = a ^ 0x1b
+    return p
 
 # This is the main function
 def main():
@@ -28,7 +63,8 @@ def main():
     # Perform the attack
 
     # Print the key
-
+    p = gf8Multiply(0x53, 0xCA)
+    print ("p is: " + str(p))
     # Print the number of oracle interactions required
     print "Total oracle interactions: " + str(interactions)
 
