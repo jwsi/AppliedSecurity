455a5146aad62cba7bc88164d0e75c2d03771217
James Webb <james@ultra-horizon.com>
2018-04-05 23:39:27 +0100

    Calculated delta 1 values

--- cw2/fault/attack.py
+++ cw2/fault/attack.py
@@ -10,17 +10,25 @@ multiplyTable = []
 # This function communicates with the attack target
 def communicate(target, message, fault):
     global interactions
-    # Send label & ciphertext to attack target.
+    # Send fault + plaintext to attack target.
+    if fault is None:
+        faultString = ""
+    else:
+        faultString = fault.description()
     messageString  = "{0:X}".format(message)
-    faultString = fault.description()
     target.stdin.write(faultString + "\n")
     target.stdin.write(messageString  + "\n")
     target.stdin.flush()
 
-    # Receive result code from attack target.
-    ctxt = int(target.stdout.readline().strip())
+    # Receive ciphertext from attack target.
+    ctxt = int(target.stdout.readline().strip(), 16)
     interactions += 1
     return ctxt
+
+
+# Given a ciphertext it returns the corresponding byte block
+def getBlock(ctxt, number):
+    return (ctxt >> (128-(number*8))) & 0xFF
 
 
 def generateMessages(amount):
@@ -68,15 +76,41 @@ def _gf28Multiply(a, b):
     return p
 
 
+# From the paper this calculates the possible set of delta 1 values (Section 3.1)
+def delta1(x, xF):
+    possibilities = []
+    for k1 in range(256):
+        line1 = sInv[ getBlock(x, 1) ^ k1 ] ^ sInv[ getBlock(xF, 1) ^ k1 ]
+        delta1 = multiplyTable[line1][141] # 2^-1 = 141 in Rijndael GF2^8 field
+
+        for k14 in range(256):
+            line2 = sInv[ getBlock(x, 14) ^ k14 ] ^ sInv[ getBlock(xF, 14) ^ k14 ]
+            if line2 == delta1:
+
+                for k11 in range(256):
+                    line3 = sInv[ getBlock(x, 11) ^ k11 ] ^ sInv[ getBlock(xF, 11) ^ k11 ]
+                    if line3 == delta1:
+
+                        for k8 in range(256):
+                            line4 = sInv[ getBlock(x, 8) ^ k8 ] ^ sInv[ getBlock(xF, 8) ^ k8 ]
+                            if multiplyTable[line4][246] == delta1 and delta1 not in possibilities: # 3^-1 = 246 in Rijndael GF2^8 field
+                                possibilities.append(delta1)
+    return possibilities
+
+
 # This is the main function
 def main():
     # Spin up a subprocess.
     target = subprocess.Popen(args=["noah", sys.argv[1]], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     # Perform the attack
+    createMultiplyTable() # Create table for Galois Field multiplication lookup
+    ctxt = 309576198173487898485272507802272752224
+    ctxtFaulty = 213524607176099836202173306380891822739
+    print(delta1(ctxt, ctxtFaulty))
+
+
 
     # Print the key
-    createMultiplyTable()
-    print multiplyTable
     # Print the number of oracle interactions required
     print "Total oracle interactions: " + str(interactions)
 
