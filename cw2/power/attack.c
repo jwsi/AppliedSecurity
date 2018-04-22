083614a9ffd85997e4439310a2279e6a96f6d3ef
James Webb <james@ultra-horizon.com>
2018-04-22 16:22:04 +0100

    broken c implementation

--- cw2/power/attack.c
+++ cw2/power/attack.c
@@ -1,8 +1,7 @@
 #include "attack.h"
-#include "tables.h"
 
 // Define global constants here...
-#define SAMPLE_SIZE 10
+#define SAMPLE_SIZE 100
 #define GOT printf("Got to line %d\n", __LINE__)
 
 
@@ -26,8 +25,18 @@ typedef struct trace {
     char msg[32+1]; // Add extra value for null terminator
 } trace_t;
 
+
+// Define the standard power-trace length
+int STD_LENGTH;
 // Define the global traces object
 trace_t *traces;
+// Define an array to store byte x for each sector of each sample
+// uint8_t *sector_byte;
+// Define the corrolation matrix, v and h
+double **correlation;
+// uint8_t **v;
+uint8_t **h;
+uint8_t **real_power;
 
 
 
@@ -37,6 +46,7 @@ trace_t *traces;
 
 // This function interacts with the attack target and generates a trace structure
 void interact(trace_t *trace, const int block, const int sector ) {
+    // printf("sector: %d\n", sector);
     // Send block and sector to attack target...
     fprintf( target_in, "%d\n"   , block  );  fflush( target_in );
     fprintf( target_in, "%032X\n", sector );  fflush( target_in );
@@ -64,6 +74,7 @@ void interact(trace_t *trace, const int block, const int sector ) {
     trace->block  = block;
     sprintf(trace->sector, "%032x", sector);
     // Debug prints
+    // printf("%d\n", trace->length);
     // printf("%s\n", trace->msg);
     // printf("%s\n", trace->sector);
     // printf("sector: %d\n", sector);
@@ -84,38 +95,131 @@ uint8_t get_sector_byte(trace_t *trace, int byte_number){
 }
 
 
-
 // This function generates a number of power traces equal to the sample size
 void generate_traces(){
     // Allocate the global traces array based on the sample size
     traces = malloc(sizeof(trace_t) * SAMPLE_SIZE);
-    for (int i=0; i<SAMPLE_SIZE; i++){
-        interact(&traces[i], -1, i*50);
+    interact(&traces[0], -1, 0);
+    STD_LENGTH = traces[0].length;
+    for (int i=1; i<SAMPLE_SIZE; i++){
+        interact(&traces[i], -1, i);
+        if (traces[i].length != STD_LENGTH){ // Throw error if power traces not consistant length
+            printf("Power trace length is not constant!\n");
+            abort();
+        }
     }
 }
 
 
-void calculate_key1(){
-    // Iterate through each key block in the AES state table in Round Key 1
-    for (int sample=0; sample < SAMPLE_SIZE; sample++){
-        for (int byte_number=0; byte_number<16; byte_number++){
-            for (int key_byte=0; key_byte<256; key_byte++){ // Try all possible byte values
-                // xor with sector byte
-                // key_byte ^ get_sector_byte(&traces[sample], byte_number);
+// This function returns the hamming weight of a byte
+int byte_hamming_weight(uint8_t byte){
+    // Increase hweight until byte is 0
+    int hweight;
+    for (hweight=0; byte; hweight++){ // And with byte-1 until byte is zero...
+        byte = byte & byte-1;
+    }
+    return hweight;
+}
 
-                // Apply sbox
-                // Apply hamming weight
-            }
+
+void calculate_h_matrix(int byte){
+    uint8_t tmp;
+    uint8_t sector_byte;
+    for (int sample=0; sample < SAMPLE_SIZE; sample++){
+        sector_byte = get_sector_byte(&traces[sample], byte);
+        for (int key_byte=0; key_byte<256; key_byte++){ // Try all possible byte values
+            tmp = sector_byte ^ key_byte;
+            h[key_byte][sample] = byte_hamming_weight(s[tmp]);
         }
     }
-    //correlate matrix to power trace
+}
+
+//
+// void calculate_h_matrix(){
+//     for (int i=0; i<SAMPLE_SIZE; i++){
+//         for (int j=0; j<256; j++){
+//             h[j][i] = byte_hamming_weight(v[j][i]);
+//         }
+//     }
+// }
+
+void calculate_power_matrix(){
+    for (int sample=0; sample < SAMPLE_SIZE; sample++){
+        for (int value=0; value < STD_LENGTH; value++){
+            real_power[value][sample] = traces[sample].values[value];
+        }
+    }
+}
+
+
+double correlate(uint8_t *x, uint8_t *y){
+    double x_bar = 0, y_bar = 0;
+    // Calculate the mean for both classes
+    for (int i =0; i < SAMPLE_SIZE; i++){
+        x_bar += x[i];
+        y_bar += y[i];
+    }
+    x_bar = x_bar / SAMPLE_SIZE;
+    y_bar = y_bar / SAMPLE_SIZE;
+
+    // Calculate the correlation coefficient
+    double numerator = 0, denominator1 = 0, denominator2 = 0;
+    for (int i=0; i < SAMPLE_SIZE; i++){
+        numerator += (x[i] - x_bar) * (y[i] - y_bar);
+        denominator1 += pow((x[i] - x_bar), 2);
+        denominator2 += pow((y[i] - y_bar), 2);
+    }
+    return numerator / (sqrt(denominator1) * sqrt(denominator2));
+}
+
+
+void allocate_matrices(){
+    // Correlation matrix is of size STD_LENGTH x (256 * 16)
+    correlation = (double**) malloc(sizeof(double*) * STD_LENGTH);
+    for (int i=0; i<STD_LENGTH; i++){
+        correlation[i] = (double*) malloc(sizeof(double) * 256);
+    }
+    // // v matrix is of size SAMPLE_SIZE x 256
+    // v = (uint8_t**) malloc(sizeof(uint8_t*) * 256);
+    // for (int i=0; i<256; i++){
+    //     v[i] = (uint8_t*) malloc(sizeof(uint8_t) * SAMPLE_SIZE);
+    // }
+    // h matrix is of size 256 x SAMPLE_SIZE
+    h = (uint8_t**) malloc(sizeof(uint8_t*) * 256);
+    for (int i=0; i<256; i++){
+        h[i] = (uint8_t*) malloc(sizeof(uint8_t) * SAMPLE_SIZE);
+    }
+    // real_power matrix is of size STD_LENGTH x SAMPLE_SIZE
+    real_power = (uint8_t**) malloc(sizeof(uint8_t*) * STD_LENGTH);
+    for (int i=0; i<STD_LENGTH; i++){
+        real_power[i] = (uint8_t*) malloc(sizeof(uint8_t) * SAMPLE_SIZE);
+    }
 }
 
 
 // This is the main attack function
 void attack(){
-    generate_traces();
-    calculate_key1();
+    generate_traces(); // Gather traces from oracle
+    allocate_matrices(); // Allocate memory for matrices
+    calculate_power_matrix(); // Calculate a matrix for power values (x=time, y=sample)
+    // sector_byte = malloc(sizeof(uint8_t) * SAMPLE_SIZE);
+    for (int byte=0; byte<16; byte++){
+        printf("Finding key byte: %d\n", byte);
+        // calculate_v_matrix(byte);
+        calculate_h_matrix(byte);
+        double maxVal=0;
+        uint8_t calculatedKey;
+        for (int value=0; value < STD_LENGTH; value++){
+            for (int keyHyp = 0; keyHyp < 256; keyHyp++){
+                correlation[value][keyHyp] = correlate(h[keyHyp], real_power[value]);
+                if (correlation[value][keyHyp] > maxVal){
+                    maxVal = correlation[value][keyHyp];
+                    calculatedKey = keyHyp;
+                }
+            }
+        }
+        printf("Key byte: %d is %u with correlation: %f\n\n", byte, calculatedKey, maxVal);
+    }
 }
 
 
@@ -136,6 +240,24 @@ void cleanup( int s ){
         free(traces[i].values);
     }
     free(traces);
+
+    // Free correlation matrix
+    for (int i=0; i<STD_LENGTH; i++){
+        free(correlation[i]);
+    }
+    free(correlation);
+    //
+    // // Free v matrix
+    // for (int i=0; i<SAMPLE_SIZE; i++){
+    //     free(v[i]);
+    // }
+    // free(v);
+
+    // Free h matrix
+    for (int i=0; i<SAMPLE_SIZE; i++){
+        free(h[i]);
+    }
+    free(h);
 
     // Forcibly terminate the attack target process.
     if( pid > 0 ) {
--- cw2/power/attack.h
+++ cw2/power/attack.h
@@ -1,7 +1,7 @@
 /* Copyright (C) 2017 Daniel Page <csdsp@bristol.ac.uk>
  *
- * Use of this source code is restricted per the CC BY-NC-ND license, a copy of 
- * which can be found via http://creativecommons.org (and should be included as 
+ * Use of this source code is restricted per the CC BY-NC-ND license, a copy of
+ * which can be found via http://creativecommons.org (and should be included as
  * LICENSE.txt within the associated archive or repository).
  */
 
@@ -17,6 +17,8 @@
 #include  <signal.h>
 #include  <unistd.h>
 #include   <fcntl.h>
+#include <math.h>
+
+#include "tables.h"
 
 #endif
-
